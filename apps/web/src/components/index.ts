1cfcb57d3c7781bb3601221933fc1a7a9a89f3ce
dladden <dladden77@gmail.com>
2026-08-02 20:21:26 -0400

    barrel

---
+++ apps/web/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as DarkMode } from './DarkMode';
+export { default as LandingNavbar } from './LandingNavbar';
+export { default as SkillCard } from './SkillCard';
+export { default as HeroConway } from './HeroConway';
